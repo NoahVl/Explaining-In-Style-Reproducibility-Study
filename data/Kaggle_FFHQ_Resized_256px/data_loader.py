120ffc7c9a5ca5e42f8251de15f4b6e8a37641d0
Noah van der Vleuten <54477889+NoahVl@users.noreply.github.com>
2022-01-13 19:48:03 +0100

    Renamed folder such that files can be imported in Python.

--- data/Kaggle_FFHQ_Resized_256px/data_loader.py
+++ data/Kaggle_FFHQ_Resized_256px/data_loader.py
@@ -4,10 +4,16 @@ import torch.utils.data as data
 import os
 from PIL import Image
 import pandas as pd
+from torchvision import transforms
 
 
 class FFHQ(data.Dataset):
     def __init__(self, root, label="gender"):
+        """
+        PyTorch DataSet for the FFHQ-Age dataset.
+        :param root: Root folder that contains a directory for the dataset and the csv with labels in the root directory.
+        :param label: Label we want to train on, chosen from the csv labels list.
+        """
         self.root = root
         self.target_class = label
 
@@ -17,6 +23,14 @@ class FFHQ(data.Dataset):
 
         # Import labels from a CSV file
         self.labels = pd.read_csv(os.path.join(self.root, "ffhq_aging_labels.csv"))
+
+        # Image transformation
+        self.transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
         # Make a lookup dictionary for the labels
         # Get column names of dataframe
@@ -29,7 +43,7 @@ class FFHQ(data.Dataset):
 
 
     def __getitem__(self, index):
-        _img = Image.open(self.images[index]).convert('RGB')
+        _img = self.transform(Image.open(self.images[index]))
         _label = self.one_hot_encoding[self.labels.iloc[index, self.class_id]]
         return _img, _label
 
@@ -45,5 +59,5 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     # Put the image class in the title
     plt.title(dataset[0][1])
-    plt.imshow(dataset[0][0])
+    plt.imshow(dataset[0][0].permute(1, 2, 0))
     plt.show()
